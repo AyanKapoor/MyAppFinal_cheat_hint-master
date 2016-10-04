7705304ac6164004089d8be86a423979a5a87a93
AyanKapoor <ayan14028@iiitd.ac.in>
2016-10-04 23:12:59 +0530

    Presentation improved

--- app/src/main/java/com/example/ayankapoor/myapplication/HelloAndroid.java
+++ app/src/main/java/com/example/ayankapoor/myapplication/HelloAndroid.java
@@ -71,13 +71,7 @@ public class HelloAndroid extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
 
 
